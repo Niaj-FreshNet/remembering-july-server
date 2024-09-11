b1ef285bb7a0ab96c85a6fab87c3b3012156c33f
Niaj-FreshNet <khushbuwaala@gmail.com>
2024-09-11 18:34:07 +0600

    commented the mongodb ping line code

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const cardCollection = client.db("july").collection("cards");
         const userCollection = client.db("july").collection("users");
@@ -282,8 +282,8 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // await client.db("admin").command({ ping: 1 });
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
